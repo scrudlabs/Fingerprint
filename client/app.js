6360139f19718e16e153d8e826a28093af9b7c5c
bibi10 <bdjafalo@outlook.com>
2019-02-10 19:29:20 +0100

    Client side

---
+++ client/app.js
@@ -0,0 +1,53 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const uploadRouter = require('./routes/upload');
+const app = express();
+const port = 8000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+
+// app.use('/', subscribeRouter);
+
+app.use('/', uploadRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.all('*', (req, res) => {
+  res.redirect('/');
+});
+
+app.listen(process.env.PORT || port);
+
+module.exports = app;
